ab3d28f3c1bb63de0595692541a39b8933a85dd8
Jason Hogan <hogan@stanford.edu>
2023-10-17 12:55:06 -0700

    Adding zip to docs cmake

--- setup.py
+++ setup.py
@@ -14,9 +14,12 @@ except ImportError:
     )
     raise
 
+# build_type = 'RelWithDebInfo'
+build_type = 'Release'
+
 #Force sci-kit to use RelWithDebInfo build instead of Release
 #Release fails to link on the first build (for some reason) causing a crash.
-sys.argv.append('--build-type=RelWithDebInfo')
+sys.argv.append('--build-type=' + build_type)
 #print("command line args: " + str(sys.argv))
 
 from setuptools import find_packages
@@ -95,7 +98,7 @@ setup(
     package_dir={"": "src"},
     cmake_install_dir="",
 	#cmake_args=['-DCMAKE_BUILD_TYPE=RelWithDebInfo', '-DOMNIORB_PATH=C:/Users/Jason/Code/lib/omniORB-4.2.4.x64'],
-  	cmake_args=['-DCMAKE_BUILD_TYPE=RelWithDebInfo', '-DOMNIORB_PATH=' + OMNIORBBASE, '-DCMAKE_BUILD_PARALLEL_LEVEL=4', '-DCMAKE_JOB_POOL_COMPILE:STRING=compile', '-DCMAKE_JOB_POOL_LINK:STRING=link', '-DCMAKE_JOB_POOLS:STRING=compile=5;link=2'],
+  	cmake_args=['-DCMAKE_BUILD_TYPE=' + build_type, '-DOMNIORB_PATH=' + OMNIORBBASE, '-DCMAKE_BUILD_PARALLEL_LEVEL=4', '-DCMAKE_JOB_POOL_COMPILE:STRING=compile', '-DCMAKE_JOB_POOL_LINK:STRING=link', '-DCMAKE_JOB_POOLS:STRING=compile=5;link=2'],
     include_package_data=True,
     exclude_package_data={"": ["*.lib","bin/*"]},
     #data_files=[('bin', ['bin/stidevice.dll','bin/stinetwork.dll'])],
