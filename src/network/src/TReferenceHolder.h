a4e6589fbd1fee402954f7f2c05e97c328c042bd
Jason Hogan <hogan@stanford.edu>
2025-09-23 00:56:10 -0700

    Add deviceNet header inclusion to RemoteBinaryDataStream and RemoteBinaryDataStreamTarget

--- src/network/src/TReferenceHolder.h
+++ src/network/src/TReferenceHolder.h
@@ -1,7 +1,7 @@
 #ifndef STI_TNETWORK_TREFERENCEHOLDER_H
 #define STI_TNETWORK_TREFERENCEHOLDER_H
 
-#include "generated/deviceNet.h"
+#include <omniORB4/CORBA.h>
 
 #include <mutex>
 #include <vector>
