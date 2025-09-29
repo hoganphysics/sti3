6b1648d4a47d2a3b21ffe8928e7acf44e1da7055
Jason Hogan <hogan@stanford.edu>
2025-09-28 19:45:12 -0700

    Conda build working in windows

--- conda/bld.bat
+++ conda/bld.bat
@@ -1,26 +1,39 @@
 @echo on
+setlocal EnableExtensions EnableDelayedExpansion
+
+
 REM Fail fast
 IF ERRORLEVEL 1 EXIT 1
 
+set "BUILD_DIR=%SRC_DIR%\_conda_build"
+
 REM Clean build dir
-if exist "%SRC_DIR%\\_conda_build" rmdir /s /q "%SRC_DIR%\\_conda_build"
-mkdir "%SRC_DIR%\\_conda_build"
+if exist "%BUILD_DIR%" rmdir /s /q "%BUILD_DIR%"
+mkdir "%BUILD_DIR%"
 
 REM CMake needs to know where to look for deps
 set CMAKE_PREFIX_PATH=%PREFIX%;%PREFIX%\\Library
 set OPENSSL_ROOT_DIR=%PREFIX%\\Library
 
+
 REM Configure
-cmake -S "%SRC_DIR%" -B "%SRC_DIR%\\_conda_build" -G Ninja ^
+cmake -S "%SRC_DIR%" -B "%BUILD_DIR%" -G Ninja ^
   -DCMAKE_INSTALL_PREFIX="%PREFIX%" ^
   -DCMAKE_BUILD_TYPE=Release ^
   -DCMAKE_INSTALL_BINDIR=Library\\bin ^
   -DCMAKE_INSTALL_LIBDIR=Library\\lib ^
   -DCMAKE_INSTALL_INCLUDEDIR=Library\\include ^
-  -DCMAKE_INSTALL_PYTHONDIR="%SP_DIR%"
+  -DCMAKE_INSTALL_PYTHONDIR="%SP_DIR%" ^
+  -DCMAKE_MSVC_RUNTIME_LIBRARY=MultiThreadedDLL
 
-REM Build
-cmake --build "%SRC_DIR%\\_conda_build" --config Release -- -j %CPU_COUNT%
 
-REM Install
-cmake --install "%SRC_DIR%\\_conda_build" --config Release
+
+IF ERRORLEVEL 1 exit /b 1
+
+REM ----- build (Ninja: no --config) -----
+cmake --build "%BUILD_DIR%" -j %CPU_COUNT% || exit /b 1
+
+REM ----- install -----
+cmake --install "%BUILD_DIR%" || exit /b 1
+
+endlocal
