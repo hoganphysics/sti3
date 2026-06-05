65618b07330451b2952e4231d94de749cfb8e514
Jason Hogan <hogan@stanford.edu>
2026-06-04 22:15:06 -0700

    example: add TIF readWrite image channel

--- examples/python/readWrite/testDevice.py
+++ examples/python/readWrite/testDevice.py
@@ -1,5 +1,6 @@
 import itertools
 import random
+import struct
 
 import stipy
 import stipy.stidevicepy as stidevicepy
@@ -40,7 +41,8 @@ class TestDevice(stidevicepy.LocalDevice):
         # Input channel (make measurements that are recorded by the device)
         self.addInputChannel(10, stipy.MixedValueType.Double, "thermocouple voltage")   # measures a double
         self.addInputChannel(13, stipy.MixedValueType.Image, "random image (BinaryData)")   # measures an Image backed by BinaryData
-        self.addInputChannel(14, stipy.MixedValueType.Image, "random image (FileHolder)")   # measures an Image backed by FileHolder
+        self.addInputChannel(14, stipy.MixedValueType.Image, "random raw image (FileHolder)")   # measures an Image backed by a raw FileHolder
+        self.addInputChannel(15, stipy.MixedValueType.Image, "random TIF image (FileHolder)")   # measures an Image backed by a TIF FileHolder
 
         # Input/Output channels
         ch = self.addInputChannel(11, stipy.MixedValueType.Number, stipy.MixedValueType.Vector, "vector args")           #measures a number (input); accepts a vector argument (output)
@@ -53,6 +55,36 @@ class TestDevice(stidevicepy.LocalDevice):
         length = self.IMAGE_WIDTH * self.IMAGE_HEIGHT * self.IMAGE_BYTES_PER_PIXEL
         return bytes(random.getrandbits(8) for _ in range(length))
 
+    def random_tif_image_bytes(self):
+        pixels = self.random_image_bytes()
+        ifd_entry_count = 10
+        ifd_offset = 8
+        image_offset = ifd_offset + 2 + (ifd_entry_count * 12) + 4
+
+        def ifd_entry(tag, field_type, count, value):
+            if field_type == 3 and count == 1:
+                value_bytes = struct.pack("<H", value) + b"\x00\x00"
+            else:
+                value_bytes = struct.pack("<I", value)
+            return struct.pack("<HHI", tag, field_type, count) + value_bytes
+
+        entries = [
+            ifd_entry(256, 4, 1, self.IMAGE_WIDTH),       # ImageWidth, LONG
+            ifd_entry(257, 4, 1, self.IMAGE_HEIGHT),      # ImageLength, LONG
+            ifd_entry(258, 3, 1, 8),                      # BitsPerSample, SHORT
+            ifd_entry(259, 3, 1, 1),                      # Compression: none
+            ifd_entry(262, 3, 1, 1),                      # PhotometricInterpretation: BlackIsZero
+            ifd_entry(273, 4, 1, image_offset),           # StripOffsets, LONG
+            ifd_entry(277, 3, 1, 1),                      # SamplesPerPixel, SHORT
+            ifd_entry(278, 4, 1, self.IMAGE_HEIGHT),      # RowsPerStrip, LONG
+            ifd_entry(279, 4, 1, len(pixels)),            # StripByteCounts, LONG
+            ifd_entry(284, 3, 1, 1),                      # PlanarConfiguration: chunky
+        ]
+
+        header = struct.pack("<2sHI", b"II", 42, ifd_offset)
+        ifd = struct.pack("<H", ifd_entry_count) + b"".join(entries) + struct.pack("<I", 0)
+        return header + ifd + pixels
+
     def binary_data_backed_image(self):
         return stipy.Image(
             stipy.BinaryData(self.random_image_bytes()),
@@ -60,9 +92,8 @@ class TestDevice(stidevicepy.LocalDevice):
             self.IMAGE_HEIGHT,
         )
 
-    def file_holder_backed_image(self):
-        payload = self.random_image_bytes()
-        filename = "readWrite-random-image-" + str(next(self.image_measurement_index)) + ".raw"
+    def file_holder_backed_image(self, payload, extension):
+        filename = "readWrite-random-image-" + str(next(self.image_measurement_index)) + extension
         file_holder = self.makeVirtualFileHolder("", filename)
 
         if file_holder is None or not file_holder.openFile():
@@ -74,7 +105,15 @@ class TestDevice(stidevicepy.LocalDevice):
         finally:
             file_holder.closeFile()
 
-        return stipy.Image(file_holder, self.IMAGE_WIDTH, self.IMAGE_HEIGHT)
+        image = stipy.Image(file_holder, self.IMAGE_WIDTH, self.IMAGE_HEIGHT)
+        image.setFileID(file_holder.getID())
+        return image
+
+    def raw_file_holder_backed_image(self):
+        return self.file_holder_backed_image(self.random_image_bytes(), ".raw")
+
+    def tif_file_holder_backed_image(self):
+        return self.file_holder_backed_image(self.random_tif_image_bytes(), ".tif")
     
     def writeChannel(self, channel, value):
         success = False
@@ -133,8 +172,11 @@ class TestDevice(stidevicepy.LocalDevice):
             print("Read ch 13: random image (BinaryData)")
             return self.binary_data_backed_image()
         elif channel == 14:
-            print("Read ch 14: random image (FileHolder)")
-            return self.file_holder_backed_image()
+            print("Read ch 14: random raw image (FileHolder)")
+            return self.raw_file_holder_backed_image()
+        elif channel == 15:
+            print("Read ch 15: random TIF image (FileHolder)")
+            return self.tif_file_holder_backed_image()
 
         return None
 
@@ -181,6 +223,9 @@ print("Measurement 4: " + str(data))
 
 data = device.read(14)
 print("Measurement 5: " + str(data))
+
+data = device.read(15)
+print("Measurement 6: " + str(data))
 
 
 nameServiceAddr = "192.168.1.242:2809"   #OmniORB NameService
